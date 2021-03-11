15cc6e2497a77179f8770b01ff8534225f777304
80315910+gusrud0423@users.noreply.github.com <80315910+gusrud0423@users.noreply.github.com>
2021-03-11 11:59:38 +0900

    자동차 구매 가격 예측_app

--- car-price2/eda_app2.py
+++ car-price2/eda_app2.py
@@ -29,4 +29,73 @@ def run_eda_app():
         st.dataframe( car_df.corr() )
 
 
+    # 컬럼 가져와서 선택한것만 보이게 하라 
+    columns = car_df.columns
+    columns = list(columns)
+
+    selected_columns = st.multiselect('컬럼을 선택하시오', columns)
+    if len(selected_columns) != 0 :
+        st.dataframe( car_df[selected_columns] )   # empty 안나오게
+    #st.dataframe(car_df[selected_columns]) 
+
+    
+    # 상관관계 분석 화면에 보여주도록 만듦
+    # 멀티셀렉트에 컬럼명을 보여주고,
+    # 해당 컬럼들에 대한 상관계수를 보여주세요
+    # 단, 컬럼들은 숫자컬럼들만 멀티셀렉트에 나타나야 됨
+    
+    # 컬럼만 가져와서는 그 컬럼이 숫자인지 문자인지 모르니 dtype 해야함
+    print(car_df.dtypes == float)
+    print( car_df.dtypes ==  int )
+
+    corr_columns = car_df.columns[ car_df.dtypes != object ]
+    selected_corr = st.multiselect('상관계수 컬럼 선택', corr_columns)
+    
+    if len(selected_corr) > 0 :
+
+        st.dataframe(car_df[selected_corr].corr())
+        
+        # 위에서 선택한 컬럼들을 이용해서, seaborn 에 페어플롯을 그린다
+        # if 문 안에서 해야 선택한 컬럼들을 가지고 할수있다
+        
+        fig = sns.pairplot(data =  car_df[selected_corr])
+        st.pyplot(fig)
+
+
+    else :
+        st.write('선택한 컬럼이 없습니다')
+
+
+        # 컬럼을 하나만 선택하면, 해당 컬럼의 min 과 max 에 
+        # 해당하는 사람의 데이터를 화면에 보여주는 기능 개발
+    number_columns = car_df.columns[ car_df.dtypes != object ]
+    selected_col = st.selectbox( '컬럼 선택', number_columns ) 
+
+    min_data =  car_df[selected_col] == car_df[selected_col].min() 
+    st.write('최소값 데이터') 
+    st.dataframe( car_df.loc[ min_data ] )
+
+    max_data =  car_df[selected_col] == car_df[selected_col].max() 
+    st.write('최소값 데이터') 
+    st.dataframe( car_df.loc[ max_data ] )
+
+
+
+        # 고객 이름을 검색할 수 있는 기능 개발 
+    # 유저한테 검색어 받자    
+    word = st.text_input('고객의 이름을 검색하세요')
+    
+    # 검색어를 데이터프레임 customer name에서 검색하자
+    result =  car_df.loc[car_df['Customer Name'].str.contains(word, case = False),  ]
+                                                    # contains 의 case 파라미터를 쓰면 대소문자 구분 없이 포함하는것 가져옴
+    # 화면에 결과 보기                                                                        
+    st.dataframe(result)
+    
+    
+
+
+
+
+   
+
     
--- car-price2/ml_app2.py
+++ car-price2/ml_app2.py
@@ -12,38 +12,87 @@ from tensorflow.keras.layers import Dense, Dropout
 import h5py
 from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger
 import pickle
+import joblib 
 
 # 여기서 모델 불러오자 
 
 def run_ml_app() :
     st.subheader('Machine Learning')
 
-    model =  tensorflow.keras.models.load_model('data2/my_model.h5')
+    #1. 유저한테 입력을 받는다
+    # 성별
+    # 남자 버튼 누르면 남자데이터 , 여자 버튼 누르면 여자데이터가 나오도록
+    gender =  st.radio('성별을 선택하세요', ['남자', '여자'])
+    if gender == '남자' : 
+        gender = 1
 
-    # 다른데서 예측하기 위해 가져가서 쓸려면  y_pred 값만 본다
+    # 나이                             # 최소 몇~~ 부터 최대 몇 ~ 까지 범위지정
+    age =  st.number_input('나이 입력', min_value=0, max_value= 100)
 
-    # 잘 만든것인지는 코랩에서 가져온 파일결과와 같은지 확인하면 된다 
+    # salary                              
+    salary =  st.number_input('연봉 입력', min_value= 0)
 
-    new_data = np.array( [ 0, 38, 90000, 2000, 50000 ] )
+    # debt
+    debt = st.number_input('카드빚 입력', min_value=0)
 
+    # 자산
+    worth = st.number_input('자산입력', min_value=0)
+
+    # 2. 예측한다
+    # 2-1. 모델 불러오기
+    model =  tensorflow.keras.models.load_model('data2/car_ai.h5')
+
+    # 2-2. 넘파이 어레이 만든다 
+    new_data = np.array( [ gender, age, salary, debt, worth ] )
+
+    # 2-3. 피처 스케일링 하자
     new_data = new_data.reshape(1,-1)
-
-    new_data = sc.transform(new_data)
-
     sc_X = joblib.load('data2/sc_X.pk1')
+    new_data = sc_X.transform(new_data)
 
-    new_data = sc_X.transform(new_data)  # 이게 어떤 스케일러인지 모름 >> 오류남
-
+    # 2-4. 예측한다
     y_pred =  model.predict(new_data)
 
-    st.write( y_pred[0][0] )
-    # pip install scikit-learn==0.23.2 다운 받아야 실행 가능 
-
+    # 예측 결과는 스케일링 된 결과이므로 다시 돌려야 한다
+    # st.write( y_pred[0][0] )
     sc_y =  joblib.load('data2/sc_y.pk1')
 
     y_pred_orginal = sc_y.inverse_transform(y_pred)
 
-    st.write(y_pred_orginal)
+    #st.write(y_pred_orginal)
 
 
+    #3. 결과를 화면에 보여준다
+    button =  st.button('결과보기')
+    if button :
+        st.write('예측 결과입니다 {:,.2f} 달러의 차를 살 수 있습니다'.format(y_pred_orginal[0,0]))
+                              # 천단위마다 콤마 찍어서 나타내라
+                              # 소수점 밑으로 2자리까지만 나타내라
 
+
+    # 처음 설정했던것 
+    # new_data = np.array( [ 0, 38, 90000, 2000, 50000 ] )
+
+    # new_data = new_data.reshape(1,-1)
+
+    # # 코랩이랑 vscode 는 서로 다른 환경이므로 변수를 다시 지정해 줘야한다 
+    # sc_X = joblib.load('data2/sc_X.pk1')
+
+    # new_data = sc_X.transform(new_data)  # 이게 어떤 스케일러인지 모름 >> 오류남
+
+    # y_pred =  model.predict(new_data)
+
+    # st.write( y_pred[0][0] )
+    # # pip install scikit-learn==0.23.2 다운 받아야 실행 가능 
+
+    # # 코랩이랑 vscode 는 서로 다른 환경이므로 변수를 다시 지정해 줘야한다 
+    # sc_y =  joblib.load('data2/sc_y.pk1')
+
+    # y_pred_orginal = sc_y.inverse_transform(y_pred)
+
+    # st.write(y_pred_orginal)
+
+
+    # # 다른데서 예측하기 위해 가져가서 쓸려면  y_pred 값만 본다
+
+# 잘 만든것인지는 코랩에서 가져온 파일결과와 같은지 확인하면 된다 
